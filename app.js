b8511d97d6569cfbc56d1af5c943c938c7ad4192
WinstonChin <s10266664@connect.np.edu.sg>
2025-11-08 21:14:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,39 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/Accounts/views/login.html");
-});
-app.use(express.static(__dirname + '/Accounts/views')); // serve HTML/JS
 
+// Serve static files
+app.use(express.static(__dirname)); // index.html, index.css, images, etc.
+app.use('/Accounts/views', express.static(path.join(__dirname, 'Accounts/views'))); // login.html, signup.html
 
-// login imports
+// ----- LOGIN ROUTES -----
 const { loginUser, getUserById, updateUser, deleteUser } = require('./Accounts/login/loginController');
 const { validateLogin } = require('./Accounts/login/loginValidation');
 const { authenticate } = require('./Accounts/login/authenticate');
 
-
-// Signup imports
-const { signupUser } = require('./Accounts/signup/signupController');
-const { validateSignup } = require('./Accounts/signup/signupValidation');
-
-// Routes
-
-// LOGIN
 app.post('/login', validateLogin, loginUser);
 app.get('/user/:id', authenticate, getUserById);
 app.put('/user/:id', authenticate, updateUser);
 app.delete('/user/:id', authenticate, deleteUser);
 
-// SIGNUP
+// ----- SIGNUP ROUTES -----
+const { signupUser } = require('./Accounts/signup/signupController');
+const { validateSignup } = require('./Accounts/signup/signupValidation');
+
 app.post('/signup', validateSignup, signupUser);
 
-// Serve HTML pages
-app.get('/login', (req, res) => res.sendFile(__dirname + '/Accounts/views/login.html'));
-app.get('/signup', (req, res) => res.sendFile(__dirname + '/Accounts/views/signup.html'));
+// ----- HTML PAGES -----
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'Accounts/views/login.html')));
+app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'Accounts/views/signup.html')));
 
+// Start server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`✅ Server running on http://localhost:${port}`);
 });
-
-
-
