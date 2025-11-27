bd70f39c5aa3024545321ae1b109f7a7f40d566b
WinstonChin <s10266664@connect.np.edu.sg>
2025-11-27 12:22:11 +0800

    Add files via upload

---
+++ public/Volunteer/friends.js
@@ -0,0 +1,27 @@
+async function loadVolunteers() {
+    const res = await fetch("http://localhost:3000/community/browse/volunteers", {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`   // ADDED
+        }
+    });
+
+    const list = await res.json();
+
+    const container = document.querySelector(".people-scroll");
+    container.innerHTML = "";
+
+    list.forEach(v => {
+        container.innerHTML += `
+        <div class="people-card">
+            <img src="./default_user.png" class="people-avatar">
+            <h4 class="people-name">${v.name}</h4>
+            <button class="btn-add">Add Friend</button>
+        </div>`;
+    });
+}
+document.addEventListener("DOMContentLoaded", () => {
+    loadPosts();
+    loadVolunteers();   // ← THIS loads the friends/volunteers list
+    loadInstitutions();
+});
