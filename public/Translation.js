21629a25acb2218238337cd84e2e2727d7e4eb33
FENGFANCHEN-012 <Chensail@outlook.com>
2025-12-23 16:05:19 +0800

    #	modified:   public/Translation.js

--- public/Translation.js
+++ public/Translation.js
@@ -56,6 +56,9 @@ async function translatePage(targetLang) {
   if (translationInProgress) return;
   translationInProgress = true;
 
+  console.log(`Translating page to ${targetLang}...`);
+
+
   const apiURL = "/translate";
   const textNodes = getTextNodes(document.body);
 
