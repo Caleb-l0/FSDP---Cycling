a418866cc51cb6b5e7c2f58186747b7ecfbd725e
FENGFANCHEN-012 <Chensail@outlook.com>
2026-01-16 16:53:07 +0800

    bug fixed

--- public/Instituition/homepage_login_instituition.js
+++ public/Instituition/homepage_login_instituition.js
@@ -1,5 +1,6 @@
-
-
+const organizationId = null;
+const token = localStorage.getItem("token");
+const role = localStorage.getItem("role");
 
 if (!token || role !== "institution") {
   alert("You do not have access to this page.");
