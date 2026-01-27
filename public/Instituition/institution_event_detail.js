f67c816ed641164a74d17893d889e5c610060ee7
FENGFANCHEN-012 <Chensail@outlook.com>
2026-01-27 21:18:12 +0800

    bug fix

--- public/Instituition/institution_event_detail.js
+++ public/Instituition/institution_event_detail.js
@@ -671,7 +671,7 @@ async function assignEventHead(modal) {
     submitBtn.disabled = true;
     submitBtn.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Assigning...';
 
-    const response = await fetch(`${API_BASE}/organization/events/assign-head/${eventId}/assign-head`, {
+    const response = await fetch(`${API_BASE}/organization/events/assign-head/${eventId}`, {
       method: 'PUT',
       headers: {
         'Authorization': `Bearer ${token}`,
